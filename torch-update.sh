251dc1cee7f98942c6304e49446d3425cd5f982a
Tom Ritchford <tom@swirly.com>
2024-05-17 11:16:16 +0000

    Add envs that set root dir, git user, conda env

--- torch-update.sh
+++ torch-update.sh
@@ -3,7 +3,8 @@ set -e
 
 PKGS=(pytorch torch-data torch-vision torch-text torch-audio torchbenchmark)
 
-cd ~/git
+cd ${PYTORCH_BUILD_DIRECTORY:=~/git$PYTORCH_BUILD_SUFFIX}
+
 for pkg in ${PKGS[@]}; do
   if [ ! -d $pkg ]; then
     echo "Directory $pkg does not exist. Please run torch-clone.sh first!"
