b3f42d1ecb8d0b2575852c15523caefac1cb38c2
Isuru Fernando <ifernando@quansight.com>
2025-04-04 15:07:18 +0000

    activate and deactivate torch-common.sh

---
+++ deactivate-torch-common.sh
@@ -0,0 +1,30 @@
+unset NCORES
+unset MAX_JOBS
+unset CMAKE_BUILD_TYPE
+unset USE_CUDA
+unset TORCH_CUDA_ARCH_LIST
+unset USE_PRECOMPILED_HEADERS
+unset USE_PER_OPERATOR_HEADERS
+unset CCACHE_COMPRESS
+unset CCACHE_SLOPPINESS
+unset USE_KINETO                               # profiler
+unset USE_CUDNN                                # CNNs
+unset USE_FBGEMM                               # GEMMs
+unset BUILD_TEST                               # C++ tests
+unset BUILD_CAFFE2                             # caffe2
+unset BUILD_CAFFE2_OPS                         # caffe2
+unset USE_DISTRIBUTED                          # distributed
+unset USE_NCCL                                 # distributed
+unset USE_GLOO                                 # distributed
+unset USE_QNNPACK                              # quantized
+unset USE_XNNPACK                              # quantized
+unset USE_FLASH_ATTENTION
+unset USE_MEM_EFF_ATTENTION
+unset CMAKE_PREFIX_PATH
+unset CUDA_PATH
+unset CUDA_HOME
+unset CMAKE_CUDA_COMPILER
+unset CUDA_INC_PATH
+unset CMAKE_C_COMPILER_LAUNCHER
+unset CMAKE_CXX_COMPILER_LAUNCHER
+unset CMAKE_CUDA_COMPILER_LAUNCHER
