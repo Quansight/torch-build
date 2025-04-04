a2cfc08d6048a9bdfd8889999f95a7c85f567c99
Isuru Fernando <ifernando@quansight.com>
2025-04-04 15:07:18 +0000

    activate and deactivate torch-common.sh

--- torch-env.sh
+++ torch-env.sh
@@ -6,4 +6,9 @@ SCRIPT_DIR=$( pushd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd
 conda env create \
     -f $SCRIPT_DIR/pytorch-dev.yaml \
     -n ${PYTORCH_CONDA_ENV:=pytorch-dev$PYTORCH_BUILD_SUFFIX}
+
+conda activate ${PYTORCH_CONDA_ENV:=pytorch-dev$PYTORCH_BUILD_SUFFIX}}
+echo "source ~/git/torch-build/torch-common.sh"             > $CONDA_PREFIX/etc/conda/activate.d/activate-torch.sh
+echo "source ~/git/torch-build/deactivate-torch-common.sh"  > $CONDA_PREFIX/etc/conda/activate.d/deactivate-torch.sh
+
 popd
