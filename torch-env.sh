a405cfd450aa644303fff10d006a3d95c6902718
Benjamin Glass <bglass@openteams.com>
2025-06-17 13:43:19 -0500

    Fix typo in torch-env.sh
    Remove an extra bracket in torch-env.sh.

--- torch-env.sh
+++ torch-env.sh
@@ -7,7 +7,7 @@ conda env create \
     -f $SCRIPT_DIR/pytorch-dev.yaml \
     -n ${PYTORCH_CONDA_ENV:=pytorch-dev$PYTORCH_BUILD_SUFFIX}
 
-conda activate ${PYTORCH_CONDA_ENV:=pytorch-dev$PYTORCH_BUILD_SUFFIX}}
+conda activate ${PYTORCH_CONDA_ENV:=pytorch-dev$PYTORCH_BUILD_SUFFIX}
 echo "source $SCRIPT_DIR/torch-common.sh"             > $CONDA_PREFIX/etc/conda/activate.d/activate-torch.sh
 echo "source $SCRIPT_DIR/deactivate-torch-common.sh"  > $CONDA_PREFIX/etc/conda/activate.d/deactivate-torch.sh
 
