427bec818f24a147a1a8f21a489d453d7f4cdf44
Benjamin Glass <bglass@openteams.com>
2026-07-09 16:34:35 +0000

    Switch to using BLIS for BLAS (except on Mac)
    BLIS is substantively faster than MKL on AMD CPUs, which represents all
    of our dev servers. Switch over.

--- torch-env.sh
+++ torch-env.sh
@@ -15,5 +15,13 @@ fi
 
 eval "$(conda shell.bash hook)"
 conda activate ${PYTORCH_CONDA_ENV:=pytorch-dev${PYTORCH_BUILD_SUFFIX}}
+
+# Pin the BLAS type, so that future package installs don't inadvertently switch it.
+if [[ $OSTYPE =~ darwin ]]; then
+  echo "libblas=*=*_newaccelerate" >> $CONDA_PREFIX/conda-meta/pinned
+else
+  echo "libblas=*=*_blis" >> $CONDA_PREFIX/conda-meta/pinned
+fi
+
 echo "source $SCRIPT_DIR/torch-common.sh" > $CONDA_PREFIX/etc/conda/activate.d/activate-torch.sh
 echo "source $SCRIPT_DIR/deactivate-torch-common.sh" > $CONDA_PREFIX/etc/conda/deactivate.d/deactivate-torch.sh
